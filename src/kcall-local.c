c8454130cc5748a5aa89f66cadbb13f5199f0f1b
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2019-05-10 12:45:13 +0200

    Enhancement: Multithread Local Kcall Benchmark

--- src/kcall-local.c
+++ src/kcall-local.c
@@ -28,37 +28,114 @@
 #ifdef __benchmark_kcall_local__
 
 /**
- * @brief Benchmarks a remote Kernel Call
+ * @name Benchmark Parameters
+ */
+/**@{*/
+#define NTHREADS_MIN                1  /**< Minimum Number of Working Threads      */
+#define NTHREADS_MAX  (THREAD_MAX - 1) /**< Maximum Number of Working Threads      */
+#define NTHREADS_STEP               1  /**< Increment on Number of Working Threads */
+/**@}*/
+
+/**
+ * @brief Thread info.
+ */
+struct tdata
+{
+	int tnum;  /**< Thread Number */
+} tdata[NTHREADS_MAX] ALIGN(CACHE_LINE_SIZE);
+
+/**
+ * @name Benchmark Kernel Parameters
+ */
+/**@{*/
+static int NTHREADS; /**< Number of Working Threads */
+/**@}*/
+
+/**
+ * @brief Issues a local kernel call.
+ */
+static void *task(void *arg)
+{
+	struct tdata *t = arg;
+	uint64_t t0, t1;
+
+	for (int i = 0; i < NITERATIONS + SKIP; i++)
+	{
+		perf_start(0, PERF_DCACHE_STALLS);
+		perf_start(1, PERF_ICACHE_STALLS);
+		t0 = stopwatch_read();
+
+			syscall0(NR_thread_get_id);
+
+		t1 = stopwatch_read();
+		perf_stop(1);
+		perf_stop(0);
+
+		if (i >= SKIP)
+		{
+			printf("%s %d %s %d %s %d %s %d %s %d %s %d",
+				"[benchmarks][kcall_local]",
+				i - SKIP,
+				"nthreads",
+				NTHREADS,
+				"tnum",
+				t->tnum,
+				"cycles",
+				UINT32(stopwatch_diff(t0, t1)),
+				"d-stalls",
+				UINT32(perf_read(0)),
+				"i-stalls",
+				UINT32(perf_read(1))
+			);
+		}
+	}
+
+	return (NULL);
+}
+
+/**
+ * @brief Local Kernel Call Benchmark Kernel
+ *
+ * @param nthreads Number of working threads.
+ */
+static void kernel_kcall_local(int nthreads)
+{
+	kthread_t tid[NTHREADS_MAX];
+
+	/* Save kernel parameters. */
+	NTHREADS = nthreads;
+
+	/* Spawn threads. */
+	for (int i = 0; i < nthreads; i++)
+	{
+		/* Initialize thread data structure. */
+		tdata[i].tnum = i;
+
+		kthread_create(&tid[i], task, &tdata[i]);
+	}
+
+	/* Wait for threads. */
+	for (int i = 0; i < nthreads; i++)
+		kthread_join(tid[i], NULL);
+}
+
+/**
+ * @brief Local Kernel Call Benchmark
+ *
+ * @param size Transfer size.
  */
 void benchmark_kcall_local(void)
 {
-	uint64_t reg;
+#ifndef NDEBUG
 
-	/*
-	 * TODO: Query performance monitoring capabilities.
-	 */
+	kernel_kcall_local(NTHREADS_MAX);
 
-	for (size_t j = 0; j < ARRAY_LENGTH(perf_events); j++)
-	{
-		for (int i = SKIP; i < NITERATIONS + SKIP; i++)
-		{
-			nanvix_perf_start(0, perf_events[j].num);
+#else
 
-				syscall0(NR_thread_get_id);
+	for (int nthreads = NTHREADS_MIN; nthreads <= NTHREADS_MAX; nthreads += NTHREADS_STEP)
+		kernel_kcall_local(nthreads);
 
-			nanvix_perf_stop(0);
-			reg = nanvix_perf_read(0);
-
-			if (i >= SKIP)
-			{
-				kprintf("[benchmarks][kcall_local] %d %s %d",
-					i - SKIP,
-					perf_events[j].name,
-					UINT32(reg)
-				);
-			}
-		}
-	}
+#endif
 }
 
 #endif /* __benchmark_kcall_local__ */
